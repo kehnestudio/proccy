05d419d49084a9b23b01782171dee8b985d66cb0
jtrdr <77273650+jtrdr@users.noreply.github.com>
2021-01-11 12:26:00 +0100

    Test Commit

---
+++ app/src/main/java/com/example/flower/TestActivityJulian.java
@@ -0,0 +1,13 @@
+package com.example.flower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivityJulian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
